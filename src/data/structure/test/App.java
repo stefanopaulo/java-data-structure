aab869075524fdc62c3e96e22d69e094da6b19d7
Stefano Souza <stefano.desouza@vmetrix.com>
2021-08-10 16:36:43 -0300

    arrays and vectors: introduction

---
+++ src/data/structure/test/App.java
@@ -0,0 +1,14 @@
+package data.structure.test;
+
+import data.structure.vector.Vector;
+
+public class App {
+
+	public static void main(String[] args) {
+		Vector vector = new Vector(5);
+		
+		int i = 0;
+		i++;
+	}
+
+}
---
+++ src/data/structure/vector/Vector.java
@@ -0,0 +1,9 @@
+package data.structure.vector;
+
+public class Vector {
+	private String[] elements;
+	
+	public Vector(int capacity) {
+		this.elements = new String[capacity];
+	}
+}

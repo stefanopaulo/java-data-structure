1e12a825ffc0111f95b5acc9807a9d2d68d27704
Stefano Paulo <stefanopaulo47@gmail.com>
2021-08-29 15:19:17 -0300

    ArrayList

--- src/data/structure/test/App3.java
+++ src/data/structure/test/App3.java
@@ -1,17 +1,46 @@
 package data.structure.test;
 
-import data.structure.vector.Vector;
+import java.util.ArrayList;
 
 public class App3 {
 
 	public static void main(String[] args) {
 
-		Vector<String> vector = new Vector<String>(3);
+		ArrayList<String> arrayList = new ArrayList<String>();
 		
-		vector.add("Element 1");
+		arrayList.add("A");
+		arrayList.add("C");
 		
-		System.out.println(vector);
+		System.out.println(arrayList);
 		
+		arrayList.add(1, "B");
+		
+		System.out.println(arrayList);
+		
+		boolean exists = arrayList.contains("A");
+		
+		if (exists) {
+			System.out.println("Elemento existe no array.");
+		} else {
+			System.out.println("Elemento não existe no array.");
+		}
+		
+		int pos = arrayList.indexOf("D");
+		
+		if (pos > -1) {
+			System.out.println("Elemento existe no array.");
+		} else {
+			System.out.println("Elemento não existe no array.");
+		}
+		
+		System.out.println(arrayList.get(2));
+		
+		arrayList.remove(0);
+		arrayList.remove("B");
+		
+		System.out.println(arrayList);
+		
+		System.out.println(arrayList.size());
 	}
 
 }
